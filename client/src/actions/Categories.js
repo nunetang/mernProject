daf55c21d326abd4f7587ab5b3d2ae1ad0b371d8
Nune Tangamyan <38163311+nunetang@users.noreply.github.com>
2020-05-06 21:28:25 -0700

    Working on User Auth., added npm packages

---
+++ client/src/actions/Categories.js
@@ -0,0 +1,5 @@
+export const GET_ERRORS = "GET_ERRORS";
+
+export const GET_PROFILE = "GET_PROFILE";
+
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
---
+++ client/src/actions/authAct.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import setAuthToken from "../actions/utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { GET_ERRORS, SET_CURRENT_USER } from "./Categories";
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/auth/user/register", userData)
+    .then(() => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Get User token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/auth/user/login", userData)
+    .then(res => {
+      const { token } = res.data;
+      // Set token to ls
+      sessionStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+
+// Set logged in user
+export const setCurrentUser = (decoded, dispatch) => {
+  // dispatch(getCurrentProfile());
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from sessionStorage
+  sessionStorage.removeItem("jwtToken");
+
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
