60d8e4457621bdd9cbfe6935158200eb6916e779
Nune Tangamyan <38163311+nunetang@users.noreply.github.com>
2020-05-18 18:14:39 -0700

    working on register and login pages

--- client/src/actions/Categories.js
+++ client/src/actions/Categories.js
@@ -1,5 +1,7 @@
 export const GET_ERRORS = "GET_ERRORS";
 
 export const GET_PROFILE = "GET_PROFILE";
-
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+
+export const FETCH_ACTIVITIES = "FETCH_ACTIVITIES";
+export const DELETE_ACTIVITY = "DELETE_ACTIVITY";
